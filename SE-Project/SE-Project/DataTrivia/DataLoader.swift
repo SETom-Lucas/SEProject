a07f6e41e9cb522783adaf57179c26419b76bcc4
Villiere-Lucas <62070618+Villiere-Lucas@users.noreply.github.com>
2022-04-26 18:03:34 +0200

    Add files via upload

---
+++ SE-Project/SE-Project/DataTrivia/DataLoader.swift
@@ -0,0 +1,74 @@
+//
+//  DataLoader.swift
+//  SE-Project
+//
+//  Created by Tom Leonard on 29/03/2022.
+
+
+import Foundation
+import SwiftUI
+//DataLoader is used to load the the questions in our jason file then parsing them as readable swift code that we can use in the rest of the application.
+
+public class DataLoader {
+    
+    @Published var questionList = [Questions]()
+    @Published private(set) var length = 0
+    @Published private(set) var index = 0
+    @Published var currentQuestion = DefaultQuestion
+    @Published private(set) var question: String = ""
+    @Published private(set) var answerChoices: [Answers] = []
+    @Published private(set) var reachedEnd = false
+    @Published private(set) var answerSelected = false
+    @Published private(set) var score = 0
+    @Published private(set) var progress: CGFloat = 0.00
+    
+    
+    init(){
+        loading()
+    }
+        func loading() {
+            guard let path = Bundle.main.path(forResource: "data", ofType: "json") else {
+                print ("here")
+                return
+            }
+            let url = URL(fileURLWithPath: path )
+                do{
+                    let data = try Data(contentsOf: url)
+                    let jsonDecoder = JSONDecoder()
+                    var dataFromJson = try jsonDecoder.decode([Questions].self, from: data)
+                    dataFromJson.shuffle() //The questions will be in a different order every time the quizz is played
+                                           
+                    self.questionList = dataFromJson // Possibility to add a limit to the amount questions in the quizz if we choose
+                    self.length = questionList.count
+                    
+                }
+                catch{
+                    print(error)
+                }
+            }
+    
+    //Function to select the next question in the quizz
+    func selectQuestion() {
+        answerSelected = false
+        progress = CGFloat(Double(index + 1) / Double(length) * 350)
+        
+        if index < length {
+            currentQuestion = questionList[index]
+            index = index + 1
+            //print(currentQuestion.questionText)
+            question = currentQuestion.questionText
+            answerChoices = currentQuestion.Answers
+        } else {
+            reachedEnd = true
+            //print("There are no more questions ! the quizz is over")
+            }
+    }
+    
+    func selectAnswer(answer : Answers){
+        answerSelected = true
+        if answer.isCorrect {
+            score += 1
+        }
+    }
+}
+
---
+++ SE-Project/SE-Project/DataTrivia/database.swift
@@ -0,0 +1,27 @@
+
+//  database.swift
+//  SE-Project
+//
+//  Created by Tom Leonard on 28/03/2022.
+
+//This file contains the structures used for parsing our Json file into swift
+import Foundation
+import SwiftUI
+
+//As not all of our questions contain images we set it as an optional variable.
+struct Questions : Codable {
+    var questionText:String
+    var img:String?
+    var Answers:[Answers]
+}
+
+//Use of a default question for initializing our list of questions
+var DefaultQuestion = Questions(questionText: "", img:"Default", Answers: [])
+
+struct Answers : Codable {
+    var id : Int
+    var text : String
+    var isCorrect :Bool
+
+}
+
