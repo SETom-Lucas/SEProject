bf517c3ccf691ca5ffc73ec3f562a338a5116d51
Tom <tom.leonard.001@student.uni.lu>
2022-05-28 19:53:58 +0200

    Hangman

---
+++ SE-Project/SE-Project/Hangman/HangmanData.swift
@@ -0,0 +1,14 @@
+//
+//  HangmanData.swift
+//  SE-Project
+//
+//  Created by Tom Leonard on 28/05/2022.
+//
+
+import Foundation
+
+
+struct Words : Codable{
+    let word : String
+    let hint : String
+}
